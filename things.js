60a56618c92b1adc4d09deabbc7c7caf0dca6a42
deejaymmm <djmmm@ukr.net>
2021-04-12 17:25:15 +0300

    styles are not included

---
+++ things.js
@@ -0,0 +1,9 @@
+module.exports.array_counter = function(array) {
+    return "В массиве находится " + array.length + " элементов!";
+};
+
+module.exports.multiply = function(x, y) {
+    return `${x} умножить на ${y} равно ${x * y}`;
+}
+
+module.exports.some_value = 451;
